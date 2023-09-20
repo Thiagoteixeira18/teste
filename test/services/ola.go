d57b5102d3f1f49dd76c9fe1a0f4ce841746402c
Thiagoteixeira18 <thiagoteixeira.epo@gmail.com>
2023-09-20 10:48:09 -0300

    adicionado table teste

--- test/services/ola.go
+++ test/services/ola.go
@@ -1,9 +1,30 @@
 package services_test
 
+import "fmt"
+
+func Principal() {
+	SomeValue := SomeValue()
+
+	addition := Sum(3, 2)
+
+	Multiplicacao := Multpy(5, -6)
+    
+	fmt.Printf("Some Value: %s, Addition: %d, multiplicacao: %d/n",
+	SomeValue, addition, Multiplicacao)
+}
+
+func SomeValue() string {
+	return "expect value"
+}
+
 func Sum(a int, b int) int {
-    return a + b
+	return a + b
 }
 
 func Multpy(a int, b int) int {
-    return a * b
+	return a * b
+}
+
+func multi(d, t int) int {
+    return d * t
 }
--- test/services/ola_test.go
+++ test/services/ola_test.go
@@ -13,6 +13,30 @@ func TestSum(t *testing.T) {
 }
 
 
-func TestMult(t *testing.T) {
+func TestMultply(t *testing.T) {
  require.Equal(t, Multpy(2, 2), 4)
+}
+
+func TestSomeValue(t *testing.T) {
+result := SomeValue()
+if result!= "expect value" {
+	t.Errorf("SomeValue() = %q, want %q", result, "expect value")
+}
+}
+
+func TestMulti(t *testing.T) {
+	tests := []struct {
+		d, t, esperado int
+	}{
+		{2, 3, 6},
+		{-2, 3, -6},
+		{2, -3, -6},
+		{0, 3, 0},
+	}
+	for _, test := range tests {
+		result := multi(test.d, test.t)
+		if result != test.esperado {
+			t.Errorf("Multi(%d, %d) = %d; want %d", test.d, test.t, result, test.esperado)
+		}
+	}
 }
