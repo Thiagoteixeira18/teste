c6f8962386dcd422d6a31822c6f2b9ca786a0bcb
Thiagoteixeira18 <thiagoteixeira.epo@gmail.com>
2023-09-25 08:54:42 -0300

    adicionado teste carteira de bitcoin

--- test/services/teste/int.go
+++ test/services/teste/int.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
@@ -103,4 +104,39 @@ type Triangulo struct {
 // Area retorna a área de um triângulo
 func (t Triangulo) Area() float64 {
 	return (t.Base * t.Altura) * 0.5
+}
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+// Carteira armazena o número de bitcoins que uma pessoa tem
+type Carteira struct {
+	saldo Bitcoin
+}
+
+// Depositar vai adicionar Bitcoins à carteira
+func (c *Carteira) Depositar(quantidade Bitcoin) {
+	c.saldo += quantidade
+}
+
+// ErroSaldoInsuficiente significa que uma carteira não tem Bitcoins suficientes para fazer uma retirada
+var ErroSaldoInsuficiente = errors.New("não é possível retirar: saldo insuficiente")
+
+// Retirar substrai alguns Bitcoins da carteira, retorna um erro se não puder ser executado
+func (c *Carteira) Retirar(quantidade Bitcoin) error {
+
+	if quantidade > c.saldo {
+		return ErroSaldoInsuficiente
+	}
+
+	c.saldo -= quantidade
+	return nil
+}
+
+// Saldo retorna o número de Bitcoins que uma carteira tem
+func (c *Carteira) Saldo() Bitcoin {
+	return c.saldo
 }
