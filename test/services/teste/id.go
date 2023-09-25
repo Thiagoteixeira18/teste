0abb589c680e39b2b5ec86b0f96db20cb34921a5
Thiagoteixeira18 <thiagoteixeira.epo@gmail.com>
2023-09-25 18:03:24 -0300

    adicionado teste id

---
+++ test/services/teste/id.go
@@ -0,0 +1,24 @@
+package services
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func Cumprimenta(escritor io.Writer, nome string) {
+	fmt.Fprintf(escritor, "Olá, %s", nome)
+}
+
+// ManipuladorMeuCumprimento diz Olá, mundo via HTTP
+func ManipuladorMeuCumprimento(w http.ResponseWriter, r *http.Request) {
+	Cumprimenta(w, "mundo")
+}
+
+func main() {
+	err := http.ListenAndServe(":5000", http.HandlerFunc(ManipuladorMeuCumprimento))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
