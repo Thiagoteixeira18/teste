fd217912843c5b64cb04cbb71a68ff9730be0f17
Thiagoteixeira18 <thiagoteixeira.epo@gmail.com>
2023-09-27 09:40:01 -0300

    adicionado teste de contexto

---
+++ test/services/teste/sync.go
@@ -0,0 +1,25 @@
+package services
+
+import "sync"
+
+type Contador struct {
+	mu    sync.Mutex
+	valor int
+}
+
+// NovoContador retorna um novo Contador
+func NovoContador() *Contador {
+	return &Contador{}
+}
+
+// Incrementa o contador
+func (c *Contador) Incrementa() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.valor++
+}
+
+// Valor retorna a contagem atual
+func (c *Contador) Valor() int {
+	return c.valor
+}
